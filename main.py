d72185e0098988a9962177b4e045f2d3d24fe3e0
Bridger Jones <bridgerjones15@gmail.com>
2021-08-09 13:27:47 -0600

    adding columns for a 50 X 6

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ from bs4 import BeautifulSoup
 r = requests.get("https://terraria.fandom.com/wiki/Banners_(enemy)")
 soup = BeautifulSoup(r.text, 'html.parser')
 list_of_banners = soup.find_all('span', {'id': re.compile(r'_Banner')})
+x_count = 1
+y_count = 1
 for banner_span in list_of_banners:
-    print(banner_span['id'])
+    print(f"{banner_span['id']}, {x_count}, {y_count}")
+    x_count += 1
+    if x_count == 51:
+        x_count = 1
+        y_count += 1
+        print("\n\n-----------------")
